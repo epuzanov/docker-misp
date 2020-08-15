0c61e516e81473ced369656621abb31750d90d91
Egor Puzanov <epuzanov@users.noreply.github.com>
2020-08-15 20:03:14 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,81 @@
+# MISP Docker images
+
+A Dockered MISP
+
+This is based on some of the work from the CoolAcid's docker build.
+
+-   Components are split out where possible
+-   Do not rely on supervisord
+-   Over writable configuration files
+-   Allows volumes for file store
+-   Cron job runs updates, pushes, and pulls - Logs go to docker logs
+-   Cron daemon can be startet within misp-fpm container or separated container
+-   Docker-Compose uses off the shelf images for Nginx, Redis and MySQL
+-   Images directly from docker hub, no build required
+-   Slimmed down images by using build stages and slim parent image, removes unnecessary files from images
+-   Build Kafka modules
+-   Contaioner can be started with read-only rootfs
+
+## Docker Tags
+
+builds the images automatically based on git tags. I try and tag using the following details
+
+***v\[MISP Version]\[Our build version]***
+
+-   MISP version is the MISP tag we're building
+-   Our build version is the iteration for our changes with the same MISP version
+
+## Getting Started
+
+### Development/Test
+
+-   A dry run will create sane default configurations
+
+-   `docker-compose up -d`
+
+-   Login to `https://localhost`
+    -   User: `admin@admin.test`
+    -   Password: `admin`
+
+-   Profit
+
+### Using the image for development
+
+Pull the entire repository, you can build the images using `docker-compose build`
+
+Once you have the docker container up you can access the container by running `docker-compose exec misp /bin/bash`.
+This will provide you with a root shell. You can use `apt update` and then install any tools you wish to use.
+Finally, copy any changes you make outside of the container for commiting to your branch. 
+`git diff -- [dir with changes]` could be used to reduce the number of changes in a patch file, however, becareful when using the `git diff` command.
+
+### Updating
+
+Updating the images should be as simple as `docker-compose pull` which, unless changed in the `docker-compose.yml` file will pull the latest built images.
+
+### Production
+-   It is recommended to specify which build you want to be running, and modify that version number when you would like to upgrade
+
+-   Use docker-compose, or some other config management tool
+
+-   Directory volume mount SSL Certs `./ssl`: `/etc/nginx/certs`
+    -   Certificate File: `cert.pem`
+    -   Certificate Key File: `key.pem`
+
+-   Directory volume mount and create configs: `/var/www/MISP/app/Config/`
+
+-   Additional directory volume mounts:
+    -   `/var/www/MISP/app/files`
+    -   `/var/www/MISP/app/tmp`
+
+## Image file sizes
+
+-   Core server(Saved: 2.6GB)
+    -   Original Image: 3.17GB
+    -   First attempt: 2.24GB
+    -   Remove chown: 1.56GB
+    -   PreBuild python modules, and only pull submodules we need: 800MB
+    -   PreBuild PHP modules: 596MB
+
+-   Modules (Saved: 737MB)
+    -   Original: 1.36GB
+    -   Pre-build modules: 656MB
---
+++ misp/files/docker-entrypoint.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+MISP_APP_CONFIG_PATH=/var/www/MISP/app/Config
+[ -z "$MYSQL_HOST" ] && MYSQL_HOST=db
+[ -z "$MYSQL_PORT" ] && MYSQL_PORT=3306
+[ -z "$MYSQL_USER" ] && MYSQL_USER=misp
+[ -z "$MYSQL_PASSWORD" ] && MYSQL_PASSWORD=example
+[ -z "$MYSQL_DATABASE" ] && MYSQL_DATABASE=misp
+[ -z "$REDIS_FQDN" ] && REDIS_FQDN=redis
+[ -z "$MYSQLCMD" ] && MYSQLCMD="mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -P $MYSQL_PORT -h $MYSQL_HOST -r -N  $MYSQL_DATABASE"
+
+init_misp_config(){
+    if [ ! -f $MISP_APP_CONFIG_PATH/routes.php ]
+    then
+        install -g www-data -o www-data $MISP_APP_CONFIG_PATH.dist/routes.php $MISP_APP_CONFIG_PATH/routes.php
+    fi
+    if [ ! -f $MISP_APP_CONFIG_PATH/bootstrap.php ]
+    then
+        install -g www-data -o www-data  $MISP_APP_CONFIG_PATH.dist/bootstrap.default.php $MISP_APP_CONFIG_PATH/bootstrap.php
+    fi
+    if [ ! -f $MISP_APP_CONFIG_PATH/core.php ]
+    then
+        install -g www-data -o www-data  $MISP_APP_CONFIG_PATH.dist/core.default.php $MISP_APP_CONFIG_PATH/core.php
+        sed -i "s/'php',/'cake',/" $MISP_APP_CONFIG_PATH/core.php
+    fi
+    if [ ! -f $MISP_APP_CONFIG_PATH/resque_config.php ]
+    then
+        install -g www-data -o www-data  /var/www/MISP/INSTALL/setup/config.php $MISP_APP_CONFIG_PATH/resque_config.php
+        sed -i "s/'localhost'/'$REDIS_FQDN'/" $MISP_APP_CONFIG_PATH/resque_config.php
+        sed -i "s/App::pluginPath('CakeResque') . 'tmp'/TMP . 'resque'/" $MISP_APP_CONFIG_PATH/resque_config.php
+    fi
+    if [ ! -f $MISP_APP_CONFIG_PATH/database.php ]
+    then
+        echo "Configure MISP | Set DB User, Password and Host in database.php"
+        install -g www-data -o www-data  $MISP_APP_CONFIG_PATH.dist/database.default.php $MISP_APP_CONFIG_PATH/database.php
+        sed -i "s/localhost/$MYSQL_HOST/" $MISP_APP_CONFIG_PATH/database.php
+        sed -i "s/db\s*login/$MYSQL_USER/" $MISP_APP_CONFIG_PATH/database.php
+        sed -i "s/db\s*password/$MYSQL_PASSWORD/" $MISP_APP_CONFIG_PATH/database.php
+        sed -i "s/'database' => 'misp'/'database' => '$MYSQL_DATABASE'/" $MISP_APP_CONFIG_PATH/database.php
+    fi
+    if [ ! -f $MISP_APP_CONFIG_PATH/email.php ]
+    then
+        install -g www-data -o www-data  $MISP_APP_CONFIG_PATH.dist/email.php $MISP_APP_CONFIG_PATH/email.php
+        sed -i -E "/\s+public .smtp = array./,/\s+public .fast = array./{;/\s+public .fast = array./!{;H;b;};x;p;x;}" $MISP_APP_CONFIG_PATH/email.php
+        sed -i -E "/\s+'transport'\s+=>\s+'Mail'/,/\s+public .smtp = array./d" $MISP_APP_CONFIG_PATH/email.php
+        sed -i "s/'localhost'/'mail'/" $MISP_APP_CONFIG_PATH/email.php
+        sed -i "s/'username'/\/\/'username'/" $MISP_APP_CONFIG_PATH/email.php
+        sed -i "s/'password'/\/\/'password'/" $MISP_APP_CONFIG_PATH/email.php
+        sed -i "s/'\(site\|you\)@localhost'/'misp-dev@admin.test'/" $MISP_APP_CONFIG_PATH/email.php
+        sed -i "s/'My Site'/'Misp DEV'/" $MISP_APP_CONFIG_PATH/email.php
+    fi
+    if [ ! -f $MISP_APP_CONFIG_PATH/config.php ]
+    then
+        echo "Configure MISP | Set defaults in config.php"
+        install -g www-data -o www-data $MISP_APP_CONFIG_PATH.dist/config.default.php $MISP_APP_CONFIG_PATH/config.php
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.redis_host" "$REDIS_FQDN"
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.baseurl" "$HOSTNAME"
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.python_bin" $(which python3)
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.tmpdir" "/var/www/MISP/app/tmp"
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.attachments_dir" "/var/www/MISP/app/files/attachments"
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.live" true
+
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.ZeroMQ_redis_host" "$REDIS_FQDN"
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.ZeroMQ_enable" true
+
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Enrichment_services_enable" true
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Enrichment_services_url" "http://misp-modules"
+
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Import_services_enable" true
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Import_services_url" "http://misp-modules"
+
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Export_services_enable" true
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Export_services_url" "http://misp-modules"
+
+        sudo -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Cortex_services_enable" false
+    fi
+}
+
+init_mysql(){
+    # Test when MySQL is ready....
+    # wait for Database come ready
+    isDBup () {
+        echo "SHOW STATUS" | $MYSQLCMD 1>/dev/null
+        echo $?
+    }
+
+    isDBinitDone () {
+        # Table attributes has existed since at least v2.1
+        echo "DESCRIBE attributes" | $MYSQLCMD 1>/dev/null
+        echo $?
+    }
+
+    RETRY=100
+    until [ $(isDBup) -eq 0 ] || [ $RETRY -le 0 ]
+    do
+        echo "Waiting for database to come up"
+        sleep 5
+        RETRY=$(( RETRY - 1))
+    done
+    if [ $RETRY -le 0 ]
+    then
+        >&2 echo "Error: Could not connect to Database on $MYSQL_HOST:$MYSQL_PORT"
+        exit 1
+    fi
+
+    if [ $(isDBinitDone) -eq 0 ]
+    then
+        echo "Database has already been initialized"
+    else
+        echo "Database has not been initialized, importing MySQL scheme..."
+        $MYSQLCMD < /var/www/MISP/INSTALL/MYSQL.sql
+    fi
+}
+
+
+if [[ "$INIT" == true ]]
+then
+    echo "Setup MySQL..." && init_mysql
+    echo "Configure MISP | Initialize misp base config..." && init_misp_config
+fi
+
+exec "$@"
